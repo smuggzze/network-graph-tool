a82cc692b1f2262a3a1fea640f43a5ec9924a277
Tarrin Curtis <tarrinc3@gmail.com>
2024-01-03 17:16:29 +0000

    added algorithm for computing the average clustering coefficient of a graph.

--- src/utils/convertToAdjList.js
+++ src/utils/convertToAdjList.js
@@ -1,19 +1,28 @@
 
 export function convertToAdjList(graph) {
     const adjList = new Map();
+    const visited = new Map();
 
     for (let node of graph.nodes) {
         adjList.set(node.id, []);
+        visited.set(node.id, new Set());
     }
 
     for (let link of graph.links) {
         const source = link.source.id;
         const target = link.target.id;
+        
+        if (!graph.isDirected) {
+            if (visited.get(target).has(source)) {
+                continue;
+            }
+
+            adjList.get(target).push(source);
+            visited.get(target).add(source);
+        }
 
         adjList.get(source).push(target);
-        if (!graph.isDirected) {
-            adjList.get(target).push(source);
-        }
+        visited.get(source).add(target);
     }
 
     return adjList;
--- src/utils/networkStatisticAlgorithms.js
+++ src/utils/networkStatisticAlgorithms.js
@@ -1,24 +1,49 @@
 import { constructUndirectedGraph } from "./constructUndirectedGraph";
 
 export class NetworkStatisticAlgorithms {
-    static avgClusteringCoefficient(graph, isDirected) {
+    static avgClusteringCoefficient(graph) {
+        let clusteringSum = 0;
 
+        for (let node of graph.keys()) {
+            const neighbours = new Set(graph.get(node));
+            const deg = graph.get(node).length;
+            
+            if (deg <= 1) {
+                continue;
+            }
+
+            let links = 0;
+            for (let n1 of neighbours) {
+                for (let n2 of graph.get(n1)) {
+                    if (neighbours.has(n2)) {
+                        links++;
+                    }
+                }
+            }
+            
+            const clusteringCo = links / (deg * (deg - 1));
+            clusteringSum += clusteringCo;
+        }
+
+        const avgClusteringCo = clusteringSum / graph.size;
+        return Math.round((avgClusteringCo + Number.EPSILON) * 1000) / 1000;
     }
 
     static avgPathLength(graph, isDirected) {
-
+        
     }
 
     static graphDensity(graph) {
+        const numNodes = graph.size;
         let numEdges = 0;
 
-        for (let key of graph.keys()) { //looping through each node
-            let degree = graph.get(key);
+        for (let node of graph.keys()) { //looping through each node
+            const degree = graph.get(node);
             numEdges = numEdges + degree.length // adding the edges connected to each node
         }
 
-        const density = numEdges / (numEdges * (numEdges - 1))
-        return Math.round(density * 100) / 100;
+        const density = numEdges / (numNodes * (numNodes - 1));
+        return Math.round((density + Number.EPSILON) * 1000) / 1000;
     }
 
     static networkDiameter(graph, isDirected) {
@@ -39,10 +64,10 @@ export class NetworkStatisticAlgorithms {
 
             while (queue.length > 0) {
                 const curNode = queue.shift();
-                for (let neighbor of graph.get(curNode)) {
-                    if (!visited.has(neighbor)) {
-                        visited.add(neighbor);
-                        queue.push(neighbor);
+                for (let neighbour of graph.get(curNode)) {
+                    if (!visited.has(neighbour)) {
+                        visited.add(neighbour);
+                        queue.push(neighbour);
                     }
                 }
             }
@@ -66,6 +91,6 @@ export class NetworkStatisticAlgorithms {
         }
         
         const avgDegree = degreeSum / graph.size;
-        return Math.round((avgDegree + Number.EPSILON) * 100) / 100;
+        return Math.round((avgDegree + Number.EPSILON) * 1000) / 1000;
     }
 }
